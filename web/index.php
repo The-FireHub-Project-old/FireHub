c55db6bf6467c5deeb13dd5d2968c6e777d08cea
Riddick <danijel.galic@live.com>
2022-06-14 10:55:48 +0200

    Added main FireHub class for bootstrapping (#6)

--- web/index.php
+++ web/index.php
@@ -11,7 +11,9 @@
  * @license OSL Open Source License version 3 - [https://opensource.org/licenses/OSL-3.0](https://opensource.org/licenses/OSL-3.0)
  *
  * @package App\Web
+ *
  * @version 1.0
+ * @version 1.1 Added main FireHub class for bootstrapping.
  *
  * @todo Remove ini settings after logger provider is implemented.
  */
@@ -19,3 +21,12 @@
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
+
+use FireHub\FireHub;
+
+/**
+ * Let there be light
+ */
+require 'phar://'.__DIR__.'/../firehub/core/firehub.phar/firehub.FireHub.php';
+
+$firehub = (new FireHub())->boot();
